f78300d541e51697586a934a9362346dd8ba0795
Darun <139690402+darunbjork@users.noreply.github.com>
2023-12-11 01:13:27 +0100

    Added PropTypes for movie data in MovieCard component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,60 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-const [movies, setMovies] = useState([
-    {
-        id: 1,
-        Title: 'Pulp Fiction',
-        Description: "A nonlinear narrative weaves together the lives of various characters, including hitmen, a boxer, and a gangster's wife, in this iconic Quentin Tarantino film.",
-        Genre: {
-          Name: 'Drama, Crime',
-          Description: 'A Quentin Tarantino crime drama with a unique narrative style.'
-        },
-        Director: {
-          Name: 'Quentin Tarantino',
-          Bio: 'Quentin Jerome Tarantino is an American filmmaker known for his distinctive style.',
-          Birth: 1963
-        },
-        ImageURL: 'https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg',
-        Featured: false
-      },
-      {
-        id: 2,
-        Title: 'Saving Private Ryan',
-        Description: 'During World War II, a group of soldiers is sent to retrieve a paratrooper whose brothers have been killed in action, leading to intense combat sequences and a profound exploration of sacrifice.',
-        Genre: {
-          Name: 'Drama, War',
-          Description: 'A Steven Spielberg war film depicting the harrowing realities of WWII.'
-        },
-        Director: {
-          Name: 'Steven Spielberg',
-          Bio: 'Steven Spielberg is a renowned filmmaker known for his diverse body of work.',
-          Birth: 1946
-        },
-        ImageURL: 'https://upload.wikimedia.org/wikipedia/en/a/ac/Saving_Private_Ryan_poster.jpg',
-        Featured: false
-      },
-      {
-        id: 3,
-        Title: 'Kill Bill: Vol. 1',
-        Description: "A former assassin, known as 'The Bride,' embarks on a vengeful journey to eliminate her former associates after they betrayed her on her wedding day.",
-        Genre: {
-          Name: 'Action, Thriller',
-          Description: "Quentin Tarantino's thrilling action-packed revenge saga."
-        },
-        Director: {
-          Name: 'Quentin Tarantino',
-          Bio: 'Quentin Jerome Tarantino is an American filmmaker celebrated for his storytelling style.',
-          Birth: 1963
-        },
-        ImageURL: 'https://upload.wikimedia.org/wikipedia/en/2/2c/Kill_Bill_Volume_1.png',
-        Featured: false
-      },
-    ]);
+const [movies, setMovies] = useState([]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
+const [selectedMovie, setSelectedMovie] = useState(null);
+useEffect(() => {
+  fetch("https://flixster-movies-7537569b59ac.herokuapp.com")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Movies from api:", data);
+      const moviesFromApi = data.docs.map((doc) => {
+        return {
+          _id: doc._id,
+          Title: doc.Title,
+          Description: doc.Description,
+          Genre: {
+            Name: doc.Genre?.Name || "",
+            Description: doc.Genre?.Description || ""
+          },
+          Director: {
+            Name: doc.Director?.Name || "",
+            Bio: doc.Director?.Bio || "",
+            Birth: doc.Director?.Birth || 0
+          },
+          ImageURL: doc.ImageURL,
+          Featured: doc.Featured || false
+        };
+      });
+      setMovies(moviesFromApi); // Setting the movies in state
+    });
+}, []);
+  
+
+  
   
     if (selectedMovie) {
       return (
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,33 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div onClick={() => {
-        onMovieClick(movie);
-      }}>
-        {movie.Title}
-      </div>
-    );
-  };
+  return (
+    <div onClick={() => {
+      onMovieClick(movie);
+    }}>
+      {movie.Title}
+    </div>
+  );
+};
+
+
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.number.isRequired
+    }).isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
