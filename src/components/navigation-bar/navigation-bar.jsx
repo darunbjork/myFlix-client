ef6747579d0311c291124d5a4f1417dd893185c4
Darun <139690402+darunbjork@users.noreply.github.com>
2024-01-10 01:16:31 +0100

    Manages movie filtering in MainView by genre search, using 'searchTerm' and 'selectedGenre' states. Displays MovieCard components that match the search criteria set by users.

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,26 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './navigation.scss';
 
 export const NavigationBar = ({ user, onLoggedOut, onGenreSearch }) => {
   const textRef = useRef(null);
-  const colors = ['#ff6347', '#ffd700', '#7fff00', '#40e0d0', '#9370db']; 
+  const colors = ['#ff6347', '#ffd700', '#7fff00', '#40e0d0', '#9370db'];
   const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    let currentIndex = 0;
-    const textElement = textRef.current;
-
-    const changeColor = () => {
-      textElement.style.color = colors[currentIndex];
-      currentIndex = (currentIndex + 1) % colors.length;
-    };
-
-    const interval = setInterval(changeColor, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   const handleLogout = () => {
     if (typeof onLoggedOut === 'function') {
@@ -28,17 +14,13 @@ export const NavigationBar = ({ user, onLoggedOut, onGenreSearch }) => {
     }
   };
 
-  // Inside NavigationBar component
-
-const handleSearch = (e) => {
-  e.preventDefault();
-  if (typeof onGenreSearch === 'function') {
-    onGenreSearch(e, searchTerm);
-    setSearchTerm(''); // Clear search term after search
-  }
-};
-
-
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (typeof onGenreSearch === 'function') {
+      onGenreSearch(searchTerm);
+      setSearchTerm(''); // Clear search term after search
+    }
+  };
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="enlarge-on-hover">
@@ -58,24 +40,24 @@ const handleSearch = (e) => {
           Explore Your Favorite Films
         </Navbar.Text>
         <Nav>
-          {user ? (
-            <>
-              <Form onSubmit={handleSearch} className="d-flex me-2">
-                <FormControl
-                  type="text"
-                  placeholder="Search by Genre"
-                  className="me-2"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)} 
-                />
-                <Button variant="outline-info" type="submit">
-                  Search
-                </Button>
-              </Form>
-              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-            </>
-          ) : (
-            <>
+        {user ? (
+          <>
+            <Form onSubmit={handleSearch} className="d-flex me-2">
+              <FormControl
+                type="text"
+                placeholder="Search by Genre"
+                className="me-2"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <Button variant="outline-info" type="submit">
+                Search
+              </Button>
+            </Form>
+            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+          </>
+        ) : (
+          <>
               <Nav.Link as={Link} to="/login">
                 Login
               </Nav.Link>
